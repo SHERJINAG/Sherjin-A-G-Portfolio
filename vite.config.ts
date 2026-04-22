7ce4ca1e72c90328e301d56a1f6288c12d277740
SHERJIN A G <146447202+SHERJINAG@users.noreply.github.com>
2026-04-22 12:41:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './', // Use relative paths for assets to work in subdirectories
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
